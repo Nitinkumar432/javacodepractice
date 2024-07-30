7f486abe8bac2dfed24cbadcbcc623669e9593b5
Nitinkumar432 <nitinraj7488204975@gmail.com>
2024-07-30 17:22:48 +0530

    hello sir

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main{
+  public static void main(String[] args){
+    System.out.println("hello world");
+    System.out.println("i will help  you");
+    System.out.println(56);
+    System.out.println(56+56+"hello");
+    // hello i a m here
+    String s="hello";
+    System.out.println(s);
+    int myNum = 15;
+System.out.println(myNum);
+int myNuum;
+myNuum = 15;
+System.out.println(myNuum);
+String firstName = "John ";
+String lastName = "Doe";
+String fullName = firstName + lastName;
+System.out.println(fullName);
+int a=45,b=56,c=56;
+System.out.println(a);
+
+  }
+}
